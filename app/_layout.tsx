8a44bcbd3b35652950a8f6c1dbb25032effe217d
kidboi666 <kidboi666@gmail.com>
2024-10-29 10:08:23 +0900

    layout 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,44 @@
 import "react-native-reanimated";
 
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
-      <Text style={styles.text}>Another piece of text!</Text>
-      <Button color="black" title="Tap me!" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="your course goal!" />
+        <Button title="add goal" />
+      </View>
+      <View style={styles.goaslContainer}>
+        <Text>list of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
-  text: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "red",
-    padding: 16,
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
-  button: {
-    backgroundColor: "black",
-    color: "#fff",
+  goaslContainer: {
+    flex: 5,
   },
 });
-
-/**
- * React Native 에서 View 컴포넌트는 컴포넌트를 담는 데 사용하는 유일한 컨테이너 컴포넌트다.
- * View, Text, Image, TextInput, ScrollView, StyleSheet, Button, Switch
- */
-
-/**
- * React Native는 css를 제공하지 않는다. 대신 두가지 방법으로 스타일을 정의함.
- * 1. 별도의 객체를 정의해서 프로퍼티로 전달
- * 2. 인라인 스타일을 추가해서 스타일 객체를 프로퍼티로 전달
- */
